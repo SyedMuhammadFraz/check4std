4c3c4a4103e73aa266728f14ff363ef344c49654
Xenax33 <saadkbr2@gmail.com>
2024-12-26 17:36:45 +0500

    sidebar updated

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -3,11 +3,13 @@ import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
 import Ribbon from "./Ribbon";
 import Footer from "./Footer";
+import Nav from "./Nav";
 
 function LandingPage() {
   return (
     <>
       <NavBar />
+      {/* <Nav/> */}
       <Ribbon/>
       <Outlet />
       <Footer/>

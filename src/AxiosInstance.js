8b5dfeebe131a11d9022963091e614cd65f55f97
Syed Muhammad Fraz <92173269+SyedMuhammadFraz@users.noreply.github.com>
2025-03-24 21:41:17 +0500

    bug fix

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -7,17 +7,17 @@ export const authServerInstance = axios.create({
 });
 
 export const connectTokenInstance = axios.create({
-  baseURL: 'https://check4std-authserverbackend-env.eba-kuzevk48.us-east-1.elasticbeanstalk.com/',
+  baseURL: 'http://check4std-authserverbackend-env.eba-kuzevk48.us-east-1.elasticbeanstalk.com/',
   withCredentials: true, // Important for sending cookies
 });
 
 // Second server instance
 export const webApiInstance = axios.create({
-  baseURL: 'https://check4std-backend-env.eba-rtquxvnp.us-east-1.elasticbeanstalk.com/api',
+  baseURL: 'http://check4std-backend-env.eba-rtquxvnp.us-east-1.elasticbeanstalk.com/api',
   withCredentials: true,
 });
 
 export const userRegisterInstance = axios.create({
-  baseURL: 'https://check4std-authserverbackend-env.eba-kuzevk48.us-east-1.elasticbeanstalk.com/api',
+  baseURL: 'http://check4std-authserverbackend-env.eba-kuzevk48.us-east-1.elasticbeanstalk.com/api',
   withCredentials: true,
 });

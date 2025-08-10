a6b426d8dfee2e9b5000f8a4e3915a9a730f1e36
Xenax33 <saadkbr2@gmail.com>
2025-08-10 16:55:57 +0500

    ensura integrated

--- src/utils/DoctorApis.js
+++ src/utils/DoctorApis.js
@@ -1,6 +1,5 @@
-import axios from "axios";
 import Cookies from "js-cookie";
-import { webApiInstance } from "../AxiosInstance";
+import { webApiInstance , webApiGraphQLInstance } from "../AxiosInstance";
 
 // Utility to get auth token (replace with your actual implementation)
 function getAuthToken() {
@@ -14,6 +13,19 @@ function getAuthToken() {
 
 // Example API call structure
 const DoctorApis = {
+  // Get doctor info and appointments via GraphQL
+  async getDoctorAppointmentsGraphQL() {
+    const token = getAuthToken();
+    const query = `query{\n  doctorInfo{\n    id,\n    dea,\n    availability{\n      id,\n      date,\n      timeSlot{\n        id,\n        startTime,\n        endTime,\n        status{\n          id,\n          lookupType,\n          lookupValue\n        }\n      }\n    },\n    appointments{\n      id,\n      timeSlot{\n        id,\n        startTime,\n        endTime,\n        status{\n          id,\n          lookupType,\n          lookupValue\n        }\n      },\n      status{\n        id,\n        lookupType,\n        lookupValue\n      },\n      patient{\n        id,\n        address,\n        city,\n        state,\n        zip,\n        country,\n        createdBy{\n          id,\n          name,\n          email,\n          phoneNumber,\n          dob\n        },\n        name,\n        dob,\n        phone,\n        primaryInsurance,\n        secondaryInsurance,\n        age,\n        email,\n        gender{\n          id,\n          lookupType,\n          lookupValue\n        }\n      }\n    }\n  }\n}`;
+    return webApiGraphQLInstance.post(
+      "/graphql",
+      { query },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+  },
+
   // Example: getDoctorProfile
   async getDoctorProfile() {
     const token = getAuthToken();
@@ -43,9 +55,32 @@ const DoctorApis = {
     return webApiInstance.get("/Doctor/prescription-page-token", {
       headers: { Authorization: `Bearer ${token}` },
     });
-  }
+  },
 
-  // Add more doctor-related API calls here, each including the auth token
+  // Get appointments for a specific doctor by ID
+  async getAppointmentsById(doctorId) {
+    const token = getAuthToken();
+    return webApiInstance.get(`/Doctor/appointments/${doctorId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  },
+
+  // Get doctor info by email
+  async getDoctorByEmail(email) {
+    const token = getAuthToken();
+    return webApiInstance.get(`/Doctor/get-by-email/${encodeURIComponent(email)}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+  },
+
+  // Get appointment by doctor id (appointment-by-doc-id/:id)
+  async getAppointmentByDocId(doctorId) {
+    const token = getAuthToken();
+    return webApiInstance.get(`/Doctor/appointment-by-doc-id/${doctorId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  },
 };
-
 export default DoctorApis;
--- src/utils/ensuraXmlTemplates.js
+++ src/utils/ensuraXmlTemplates.js
@@ -85,4 +85,118 @@ const nurseXml = `<?xml version='1.0' encoding='utf-8'?>
   </Prescription>
 </NewCropData>`;
 
-export { doctorXml, nurseXml };
+// Generate a doctor XML by replacing only the dynamic fields in the template
+function generateDoctorXml({
+  accountId = "DUMMY_ACCOUNT_ID",
+  accountName = "DUMMY_ACCOUNT_NAME",
+  siteID = "DUMMY_SITE_ID",
+  accountAddress1 = "DUMMY_ACCOUNT_ADDRESS1",
+  accountAddress2 = "DUMMY_ACCOUNT_ADDRESS2",
+  accountCity = "DUMMY_ACCOUNT_CITY",
+  accountState = "DUMMY_ACCOUNT_STATE",
+  accountZip = "DUMMY_ACCOUNT_ZIP",
+  accountZip4 = "DUMMY_ACCOUNT_ZIP4",
+  accountCountry = "DUMMY_ACCOUNT_COUNTRY",
+  accountPhone = "DUMMY_ACCOUNT_PHONE",
+  accountFax = "DUMMY_ACCOUNT_FAX",
+  locationId = "DUMMY_LOCATION_ID",
+  locationName = "DUMMY_LOCATION_NAME",
+  locationAddress1 = "DUMMY_LOCATION_ADDRESS1",
+  locationCity = "DUMMY_LOCATION_CITY",
+  locationState = "DUMMY_LOCATION_STATE",
+  locationZip = "DUMMY_LOCATION_ZIP",
+  locationCountry = "DUMMY_LOCATION_COUNTRY",
+  locationPhone = "DUMMY_LOCATION_PHONE",
+  locationFax = "DUMMY_LOCATION_FAX",
+  pharmacyContact = "DUMMY_PHARMACY_CONTACT",
+  prescriberId = "DUMMY_PRESCRIBER_ID",
+  prescriberLast = "DUMMY_PRESCRIBER_LAST",
+  prescriberFirst = "DUMMY_PRESCRIBER_FIRST",
+  prescriberDEA = "DUMMY_PRESCRIBER_DEA",
+  patientId = "DUMMY_PATIENT_ID",
+  patientLast = "DUMMY_PATIENT_LAST",
+  patientFirst = "DUMMY_PATIENT_FIRST",
+  patientAddress1 = "DUMMY_PATIENT_ADDRESS1",
+  patientCity = "DUMMY_PATIENT_CITY",
+  patientState = "DUMMY_PATIENT_STATE",
+  patientZip = "DUMMY_PATIENT_ZIP",
+  patientCountry = "DUMMY_PATIENT_COUNTRY",
+  patientCell = "DUMMY_PATIENT_CELL",
+  patientEmail = "DUMMY_PATIENT_EMAIL",
+  patientDOB = "DUMMY_PATIENT_DOB",
+  patientGender = "F"
+} = {}) {
+  return `<?xml version="1.0" encoding="utf-8"?>
+<NCScript xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://secure.newcropaccounts.com/interfaceV7">
+	<Credentials>
+		<partnerName>SatelliteHealth</partnerName>
+		<productName>SuperDuperSoftware</productName>
+		<productVersion>V5.3</productVersion>
+	</Credentials>
+	<UserRole>
+		<user>LicensedPrescriber</user>
+		<role>doctor</role>
+	</UserRole>
+	<Destination>
+		<requestedPage>compose</requestedPage>
+	</Destination>
+	<Account ID="${accountId}">
+		<accountName>${accountName}</accountName>
+		<siteID>${siteID}</siteID>
+		<AccountAddress>
+			<address1>${accountAddress1}</address1>
+			<address2>${accountAddress2}</address2>
+			<city>${accountCity}</city>
+			<state>${accountState}</state>
+			<zip>${accountZip}</zip>
+			<zip4>${accountZip4}</zip4>
+			<country>${accountCountry}</country>
+		</AccountAddress>
+		<accountPrimaryPhoneNumber>${accountPhone}</accountPrimaryPhoneNumber>
+		<accountPrimaryFaxNumber>${accountFax}</accountPrimaryFaxNumber>
+	</Account>
+	<Location ID="${locationId}">
+		<locationName>${locationName}</locationName>
+		<LocationAddress>
+			<address1>${locationAddress1}</address1>
+			<city>${locationCity}</city>
+			<state>${locationState}</state>
+			<zip>${locationZip}</zip>
+			<country>${locationCountry}</country>
+		</LocationAddress>
+		<primaryPhoneNumber>${locationPhone}</primaryPhoneNumber>
+		<primaryFaxNumber>${locationFax}</primaryFaxNumber>
+		<pharmacyContactNumber>${pharmacyContact}</pharmacyContactNumber>
+	</Location>
+	<LicensedPrescriber ID="${prescriberId}">
+		<LicensedPrescriberName>
+			<last>${prescriberLast}</last>
+			<first>${prescriberFirst}</first>
+		</LicensedPrescriberName>
+		<dea>${prescriberDEA}</dea>
+	</LicensedPrescriber>
+	<Patient ID="${patientId}">
+		<PatientName>
+			<last>${patientLast}</last>
+			<first>${patientFirst}</first>
+		</PatientName>
+		<PatientAddress>
+			<address1>${patientAddress1}</address1>
+			<city>${patientCity}</city>
+			<state>${patientState}</state>
+			<zip>${patientZip}</zip>
+			<country>${patientCountry}</country>
+		</PatientAddress>
+		<PatientContact>
+			<cellularTelephone>${patientCell}</cellularTelephone>
+			<email>${patientEmail}</email>
+		</PatientContact>
+		<PatientCharacteristics>
+			<dob>${patientDOB}</dob>
+			<gender>${patientGender}</gender>
+		</PatientCharacteristics>
+	</Patient>
+</NCScript>`;
+}
+
+export { doctorXml, nurseXml, generateDoctorXml };
